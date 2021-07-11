5038ea0372cf20221ee7116b88537389cbc6eb31
Manishchaudhary98 <85509005+Manishchaudhary98@users.noreply.github.com>
2021-07-11 14:22:30 +0530

    added Day 11 Tha

---
+++ Day 11/question.js
@@ -0,0 +1,57 @@
+let questions = [
+  {
+  numb: 1,
+  question: "Who had composed the original Ramayana?",
+  answer: "Rishi Valmiki",
+  options: [
+    "Rishi Valmiki",
+    "Tulsi Das",
+    "Sant Ek Nath",
+    "Anhinanda"
+  ]
+},
+  {
+  numb: 2,
+  question: " Lakshmana is considered to be the incarnation of whom?",
+  answer:"Sheshnag",
+  options: [
+    " Lord Vishnu",
+    "Lord Shiva",
+    "Lord Brahma",
+    "Sheshnag"
+  ]
+},
+  {
+  numb: 3,
+  question: " Ravana was a devotee of who among the following God?",
+  answer:"Shiva", 
+  options: [
+    "Vishnu",
+    "Brahma",
+    "Shiva",
+    "None of the above"
+  ]
+},
+  {
+  numb: 4,
+  question: "What was the name of Lord Rama's father?",
+  answer: "Dasaratha",
+  options: [
+    "Shalishuka",
+    "Nahapana",
+    "Rajadhiraj",
+    "Dasaratha"
+  ]
+},
+  {
+  numb: 5,
+  question:"Where in Lanka does Ravana keep Sita?",
+  answer: "Ashok Vatika",
+  options: [
+    "Panchvatika",
+    "Ashok Vatika",
+    "Kusum Vatika",
+    "Sita Palace"
+  ]
+},
+];
